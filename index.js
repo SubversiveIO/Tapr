80fe27f5c284f7700b0ac7c2330d8fe6eb45598e
Aaron Lichtman <aaronlichtman@gmail.com>
2016-10-24 22:25:00 -0500

    tested a callback function

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// First callback
+
+var axolotl = require('axolotl');
+
+var store = {
+    registrationID: new ArrayBuffer(),
+    getLocalRegistrationID: function(registrationID) {
+        return registrationID;
+    }
+};
